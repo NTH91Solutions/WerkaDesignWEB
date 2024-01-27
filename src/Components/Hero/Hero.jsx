fad3edd2197fcb2b235dc5a1463518c0c1b0082f
Nick-pc <nterhorst1@msn.com>
2024-01-27 23:19:26 +0100

    main

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
+import arrow_icon from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image.png'
 
 function Hero() {
   return (
@@ -16,8 +18,12 @@ function Hero() {
                 <p>for everyone</p>
             </div>
             <div className="hero_latest_button">
-                
+              <div>Latest Collection</div>
+              <img src={arrow_icon} alt="" />  
             </div>
+        </div>
+        <div className="hero-right">
+          <img src={hero_image} alt="" />
         </div>
     </div>
   )
