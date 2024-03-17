19f45bdc0d6e22493054c320bb872a7c2aa19c44
Nick-labtop <nterhorst1@msn.com>
2024-03-17 23:41:06 +0100

    main

--- Frontend/src/Pages/Welcome.jsx
+++ Frontend/src/Pages/Welcome.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, } from 'react'
 import './CSS/Welcome.css'
+import acountlogo from '../Components/Assets/acount_symbol.png'
 
 import { Link } from "react-router-dom"
 
@@ -26,6 +27,20 @@ const Welcome = () => {
       <div className='welcome'>
           <div className="welcome-container">
            <h1>Welcome {Username} </h1>
+           
+           <div className='welcome-home'>
+             <Link to='/'><img src={acountlogo} alt="" /><p>My Account</p></Link>
+             <p className='p1'>Acount information you can add your shipping adress, change your name or password or to delete your acount</p>
+           </div>
+           <div className='welcome-home'>
+           <p className='p1'>Acount information you can add your shipping adress, change your name or password or to delete your acount</p>
+             <Link to='/'><img src={acountlogo} alt="" /><p>My Account</p></Link>
+             
+           </div>
+           <div className='welcome-home'>
+             <Link to='/'><img src={acountlogo} alt="" /><p>My Account</p></Link>
+             <p className='p1'>Acount information you can add your shipping adress, change your name or password or to delete your acount</p>
+           </div>
           </div>  
 
       </div>
