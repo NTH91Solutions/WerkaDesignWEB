089ebdc69bba2a9ba44db40ad0464f71b9cf1a64
Nick-pc <nterhorst1@msn.com>
2024-03-19 21:22:20 +0100

    main

--- Frontend/src/Pages/Welcome.jsx
+++ Frontend/src/Pages/Welcome.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, } from 'react'
 import './CSS/Welcome.css'
 import acountlogo from '../Components/Assets/acount_symbol.png'
 import house_button from '../Components/Assets/homeicon.png'
+import cart_button from '../Components/Assets/cart_button.png'
 
 import { Link } from "react-router-dom"
 
@@ -27,18 +28,11 @@ const Welcome = () => {
     return (
       <div className='welcome'>
           <div className="welcome-container">
-            <div className='welcome-user'><h1>Welcome {Username} </h1></div>
+           <div className='welcome-user'><h1>Welcome {Username} </h1></div>
            <div className='welcome-home'>
-             <Link className='link' to='/'><img src={house_button} alt="" /></Link>
-             <Link to='/' style={{textDecoration:"none", color: 'black'}} ><h2>Home</h2></Link>
-           </div>
-           <div className='welcome-home'>
-           <Link to='/'><img src={acountlogo} alt="" /><p>My Account</p></Link>
-           <p className='p1'>Acount information you can add your shipping adress, change your name or password or to delete your acount</p>
-           </div>
-           <div className='welcome-home'>
-             <Link to='/'><img src={acountlogo} alt="" /><p>My Account</p></Link>
-             <p className='p1'>Acount information you can add your shipping adress, change your name or password or to delete your acount</p>
+             <button><Link to='/'style={{textDecoration:"none", color: 'black'}} ><img src={house_button} alt="" /><p>Home</p></Link></button>
+             <button><Link to='/cart'style={{textDecoration:"none", color: 'black'}} ><img src={cart_button} alt="" /><p>Cart</p></Link></button>
+             <button><Link to='/'style={{textDecoration:"none", color: 'black'}} ><img src={acountlogo} alt="" /><p>My Account</p></Link></button>
            </div>
           </div>  
 
